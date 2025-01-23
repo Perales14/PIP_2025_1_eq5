aba2bf380190d4beda3319e612fbba42b293009a
Perales14 <luisgonzallp@gmail.com>
2025-01-23 08:15:46 -0600

    First Comit

---
+++ Unidad 1/P03_EjemploLineEdit.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5 import uic, QtWidgets
+qtCreatorFile = "P03_EjemploLineEdit.ui" #Cambiar el nombre a P00 osea el numero 0, no el O XD
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        #Area de los Signals
+        self.btn_saludar.clicked.connect(self.saludar)
+
+    def saludar (self):
+        cadena = self.txt_nombre.text() ##se da en string, str, cadena, pss xd
+        if cadena != "":
+            self.msj("SALUDOS, " +cadena+", Buen Dia")
+        else:
+            self.msj("Debes de ingresar tu nombre")
+    def msj(self,txt):
+        m = QtWidgets.QMessageBox()
+        m.setText(txt)
+
+        m.exec_()
+
+    #Area de los Slots
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
---
+++ Unidad 1/POO_EjemploBoton.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5 import uic, QtWidgets
+qtCreatorFile = "POO_EjemploBoton.ui" #Cambiar el nombre a P00 osea el numero 0, no el O XD
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        #Area de los Signals
+        self.btn_saludar.clicked.connect(self.saludar)
+
+    def saludar (self):
+        print ("Saludos, al entorno de trabajo")
+
+    #Area de los Slots
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
