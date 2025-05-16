3eee5b9429e0229a4d841bb860615fd6acd73791
Perales14 <luisgonzalo.mp@gmail.com>
2025-05-16 17:11:31 -0600

    DepaP3

---
+++ Examen_Departamental-P3/Departamental/Departamental.ino
@@ -0,0 +1,80 @@
+const int pinTMP = A0;  // TMP36
+const int pinLDR = A1;  // LDR
+
+// Pines actuadores
+const int relePin = 2;
+const int motorIN1 = 7;
+const int motorIN2 = 8;
+const int motorPWM = 5;
+
+String inputString = "";
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(relePin, OUTPUT);
+  pinMode(motorIN1, OUTPUT);
+  pinMode(motorIN2, OUTPUT);
+  pinMode(motorPWM, OUTPUT);
+}
+
+void loop() {
+  float temp = leerTMP();
+  int ldr = analogRead(pinLDR);
+
+  Serial.print(temp);
+  Serial.print(",");
+  Serial.println(ldr);
+
+  if (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') {
+      procesarComando(inputString);
+      inputString = "";
+    } else {
+      inputString += c;
+    }
+  }
+
+  delay(1000);
+}
+
+float leerTMP() {
+  int rawTMP = analogRead(pinTMP);
+  float voltageTMP = rawTMP * (5.0 / 1023.0);
+  return (voltageTMP - 0.5) * 100.0;
+}
+
+void procesarComando(String comando) {
+  int focoEstado = comando.indexOf("L:") != -1 ? comando.substring(comando.indexOf("L:") + 2, comando.indexOf("T:")).toInt() : 0;
+  int motorEstado = comando.indexOf("T:") != -1 ? comando.substring(comando.indexOf("T:") + 2).toInt() : 0;
+
+  digitalWrite(relePin, focoEstado);
+
+  switch (motorEstado) {
+    case 0:
+      motor(0);
+      break;
+    case 1:
+      motor(1);
+      break;
+    case 2:
+      motor(2);
+      break;
+  }
+}
+
+void motor(int direccion) {
+  if (direccion == 0) {
+    digitalWrite(motorIN1, 0);
+    digitalWrite(motorIN2, 0);
+    analogWrite(motorPWM, 0);
+  } else if (direccion == 1) {
+    digitalWrite(motorIN1, 1);
+    digitalWrite(motorIN2, 0);
+    analogWrite(motorPWM, 200);
+  } else if (direccion == 2) {
+    digitalWrite(motorIN1, 0);
+    digitalWrite(motorIN2, 1);
+    analogWrite(motorPWM, 200);
+  }
+}
---
+++ Examen_Departamental-P3/SistemaElectronico.py
@@ -0,0 +1,62 @@
+import sys
+from PyQt5 import uic, QtWidgets
+import serial
+from PyQt5.QtCore import QTimer
+
+qtCreatorFile = "Diseño_examen.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.arduino = serial.Serial("COM10", baudrate=9600, timeout=1)
+
+        self.foco_habilitado = False
+        self.motor_habilitado = False
+
+        self.LH.clicked.connect(self.control)
+        self.TH.clicked.connect(self.control)
+
+        #timer de 1segundo, para leer y enviar datosc
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.ciclo)
+        self.timer.start(1000)
+
+    def control(self):
+        sender = self.sender()
+        if sender.objectName() == "LH":
+            self.foco_habilitado = not self.foco_habilitado
+            self.LH.setText("Deshabilitar" if self.foco_habilitado else "Habilitar")
+        elif sender.objectName() == "TH":
+            self.motor_habilitado = not self.motor_habilitado
+            self.TH.setText("Deshabilitar" if self.motor_habilitado else "Habilitar")
+
+    def ciclo(self):
+
+        foco_valor = 1 if self.foco_habilitado else 0
+        motor_valor = 1 if self.motor_habilitado else 0
+
+        mensaje = f"L:{foco_valor},T:{motor_valor}\n"
+        self.arduino.write(mensaje.encode())
+
+        # leer del arduino
+        if self.arduino.inWaiting():
+            linea = self.arduino.readline().decode().strip()
+            if linea:
+                try:
+                    temp, ldr = linea.split(",")
+                    #// conversion a % del ldr
+                    ldr_val = int(ldr)
+                    ldr_porcentaje = int((ldr_val / 1023) * 100)
+                    self.lcd_number.display(float(temp))
+                    self.progress_bar.setValue(ldr_porcentaje)
+                except Exception as e:
+                    print(f"Error al procesar datos: {e}")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
